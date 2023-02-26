a27bb3431d5f98706ab7865668528083dcbcf282
EdmundStoner <113002475+EdmundStoner@users.noreply.github.com>
2023-02-26 11:47:13 -0600

    Update LandingRate.lua

--- LandingRate.lua
+++ LandingRate.lua
@@ -136,7 +136,7 @@ lrl_logAnyWheel = lrl_boolOnGroundAny == 1 and true or false
 lrl_logAllWheels = lrl_boolOnGroundAll == 1 and true or false
 
 lrl_popupText = { "Landing Rate for Lua" .. (lrl_vr_enabled == 1 and " + VR v16" or ""), "(c)2020-2022 Dan Berry", 
-	"Mods by [erhardma] & EdmundStoner","VERSION 1.83.9" }
+	"Mods by William R Good (SparkerInVR)[erhardma] & EdmundStoner","VERSION 1.83.9" }
 lrl_showUntil = os.clock() + 5
 lrl_logDisplayOn = true
 lrl_popupState = lrl_STEERINGDN
