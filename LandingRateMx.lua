fd2a686ee6676c401533dcabd2118db50fd15bb9
Edmund <113002475+EdmundStoner@users.noreply.github.com>
2023-02-27 18:18:08 -0600

    Using gear-force over wieght instead of gforce

--- LandingRateMx.lua
+++ LandingRateMx.lua
@@ -159,8 +159,8 @@ new_table("lrl_gearForce", 50)
 lrl_logAnyWheel = lrl_boolOnGroundAny == 1 and true or false
 lrl_logAllWheels = lrl_boolOnGroundAll == 1 and true or false
 
-lrl_popupText = { "Landing Rate Max G for Lua" .. (lrl_vr_enabled == 1 and " + VR v16" or ""), "(c)2020-2022 Dan Berry", 
-	"ES VERSION 1.x", "EXPERIMENTAL" }
+lrl_popupText = { "Landing Rate Max G for Lua" .. (lrl_vr_enabled == 1 and " + VR v16" or ""), "Mod of Dan Berry's code", 
+	"ES VERSION 0.6", "EXPERIMENTAL" }
 lrl_showUntil = os.clock() + 5
 lrl_logDisplayOn = true
 lrl_popupState = lrl_STEERINGDN
